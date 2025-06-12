527ce945509544502807531efde0fd1de3c2c8dd
joseademar <joseademarti@gmail.com>
2025-06-11 21:09:56 -0300

    Praticando algoritimo e estrutura de dados e arrays

--- Algoritimos e estruturas de dados/Arrays/src/Main2.java
+++ Algoritimos e estruturas de dados/Arrays/src/Main2.java
@@ -4,10 +4,16 @@ public class Main2 {
     public static void main(String[] args) {
         SingleDimensionArray sd = new SingleDimensionArray(10);
 
-        sd.insert(0,0);
-        sd.insert(1,10);
-        sd.insert(2,20);
+        sd.insert(0,10);
+        sd.insert(1,20);
+        sd.insert(2,30);
         sd.insert(1,30);
         sd.insert(12,120);
+
+//        System.out.println("Array traversal");
+        sd.traverseArray();
+        sd.searchInArray(40);
+        int ultimoIndiceDoArray = sd.arr.length - 1;
+        System.out.println(sd.arr[ultimoIndiceDoArray]);
     }
 }
--- Algoritimos e estruturas de dados/Arrays/src/SingleDimensionArray.java
+++ Algoritimos e estruturas de dados/Arrays/src/SingleDimensionArray.java
@@ -21,4 +21,28 @@ public class SingleDimensionArray {
             System.out.println("Invalid index to access array");
         }
     }
+
+    //Traversal Array
+
+    public void traverseArray() {
+        try {
+        for (int i=0; i<arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        } catch (Exception e) {
+            System.out.println("Array no longer exists");
+        }
+    }
+
+    //Search for an element in the given Array
+
+    public void searchInArray(int valueToSearch) {
+        for (int i =0; i<arr.length; i++) {
+            if (arr[i] == valueToSearch){
+                System.out.println("Value is found at the index of " + i);
+                return;
+            }
+        }
+        System.out.println(valueToSearch + " is not found");
+    }
 }
