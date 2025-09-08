e978d7233599b826a5bfbc1a10b1ceead945e3a1
joseademar <joseademarti@gmail.com>
2025-09-08 08:26:48 -0300

    Estrutura de dados e algoritimos pesquisando um valor dentro de uma matriz

--- Algoritimos e estruturas de dados/Arrays/src/arrays/TwoDimensionArrays1.java
+++ Algoritimos e estruturas de dados/Arrays/src/arrays/TwoDimensionArrays1.java
@@ -3,8 +3,10 @@ package arrays;
 import java.util.Arrays;
 
 public class TwoDimensionArrays1 {
+    private static int[][] int2DArray;
+
     public static void main(String[] args) {
-        int[][] int2DArray;
+
         int2DArray = new int[2][2];
 
         int2DArray[0][0] = 1;
@@ -12,10 +14,24 @@ public class TwoDimensionArrays1 {
         int2DArray[1][0] = 3;
         int2DArray[1][1] = 4;
 
-        System.out.println(Arrays.deepToString(int2DArray));
+        searchingValue(3);
+
+        //System.out.println(Arrays.deepToString(int2DArray));
 
         //novo exemplo
-        String s2DArray[][] = {{"a","b"}, {"c","d"}};
-        System.out.println(Arrays.deepToString(s2DArray));
+        String s2DArray[][] = {{"a", "b"}, {"c", "d"}};
+        //System.out.println(Arrays.deepToString(s2DArray));
+    }
+
+    private static void searchingValue(int value) {
+        for (int row = 0; row < int2DArray.length; row++) {
+            for (int col = 0; col < int2DArray[0].length; col++) {
+                if (int2DArray[row][col] == value) {
+                    System.out.println("Valor encontrado na linha " + row + " coluna " + col);
+                    return;
+                }
+            }
+        }
+        System.out.println("Valor " + value + " não encontrado na Matriz");
     }
 }
