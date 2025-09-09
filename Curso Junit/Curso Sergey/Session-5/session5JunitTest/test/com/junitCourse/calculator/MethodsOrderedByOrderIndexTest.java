d5ea6299a6d9b6f2014d1a76e98177aadb452a61
joseademar <joseademarti@gmail.com>
2025-09-09 08:39:39 -0300

    Estudo sobre ciclo de vida de testes no JUnit 5

--- Curso Junit/Curso Sergey/Session-5/session5JunitTest/test/com/junitCourse/calculator/MethodsOrderedByOrderIndexTest.java
+++ Curso Junit/Curso Sergey/Session-5/session5JunitTest/test/com/junitCourse/calculator/MethodsOrderedByOrderIndexTest.java
@@ -1,35 +1,49 @@
 package com.junitCourse.calculator;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class) //Ordena a execução do métodos pela ordem dos indices.
 public class MethodsOrderedByOrderIndexTest {
+
+    StringBuilder completed = new StringBuilder("");
+
+    @AfterEach
+    void aterEach() {
+        System.out.println("The state of instance object is: " + completed);
+    }
 
     @Order(1)
     @Test
     void testB() {
         System.out.println("Running test B");
+        completed.append("1");
     }
 
     @Order(2)
     @Test
     void testA() {
         System.out.println("Running test A");
+        completed.append("2");
     }
 
     @Order(3)
     @Test
     void testD() {
         System.out.println("Running test D");
+        completed.append("3");
     }
 
     @Order(4)
     @Test
     void testC() {
         System.out.println("Running test C");
+        completed.append("4");
     }
 
 
