bdcbfa129ce842fcab3a6a55993b79cd70e72c91
joseademar <joseademarti@gmail.com>
2025-10-23 08:32:12 -0300

    developing integration test

--- Algoritimos e estruturas de dados/Exercicios/src/com/algoritimo/stringsAlgo/PrefixStringChallenge.java
+++ Algoritimos e estruturas de dados/Exercicios/src/com/algoritimo/stringsAlgo/PrefixStringChallenge.java
@@ -13,6 +13,16 @@ package com.algoritimo.stringsAlgo;
  * Prefixos de “casa” → “c”, “ca”, “cas”, “casa”
  *
  * Prefixos de “flor” → “f”, “fl”, “flo”, “flor”
+ *
+ * O while é a parte central do algoritmo.
+ * Ele verifica se o prefixo (inicialmente o primeiro elemento do array) existe no início do segundo elemento.
+ *
+ * Se não existir, indexOf(prefixo) retorna -1 e o while entra em ação, encurtando o prefixo letra por letra.
+ *
+ * Se o prefixo for encontrado no início da string, indexOf(prefixo) retorna 0, o que faz o while parar.
+ *
+ * Esse processo se repete para cada elemento do array, sempre garantindo que o prefixo seja uma substring inicial da palavra atual.
+ * O loop termina quando encontramos o maior prefixo comum entre todas as strings ou quando o prefixo se torna vazio.
  */
 
 public class PrefixStringChallenge {
@@ -30,7 +40,9 @@ public class PrefixStringChallenge {
         if (strs == null || strs.length == 0) return "";
 
         String prefixo = strs[0];
+        int elemento = prefixo.indexOf(prefixo);
         for (int i = 1; i < strs.length; i++) {
+            int result = strs[i].indexOf(prefixo);
             while (strs[i].indexOf(prefixo) != 0) {
                 prefixo = prefixo.substring(0, prefixo.length() - 1);
                 if (prefixo.isEmpty()) return "";
--- Curso Junit/Curso Sergey/Testing Controllers/UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UserControllerIntegrationTest.java
+++ Curso Junit/Curso Sergey/Testing Controllers/UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UserControllerIntegrationTest.java
@@ -1,9 +1,27 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
 
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
 //properties ={"server.port=8081", "hostname=192.168.0.2"})
@@ -12,15 +30,68 @@ import org.springframework.test.context.TestPropertySource;
 //@TestPropertySource(locations = "/application-test.properties",
 //        properties = "server.port=8081")
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@TestPropertySource(locations = "/application-test.properties")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@TestPropertySource(locations = "/application-test.properties")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserControllerIntegrationTest {
 
     @Value("${server.port}")
     private int serverPort;
 
+    @LocalServerPort
+    private int localServerPort;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    private String authorizationToken;
+
     @Test
-    void contextLoads() {
-        System.out.println("server.port" + serverPort);
+    @DisplayName("User can be created")
+    @Order(1)
+    void testCreateUser_whenValidDetailsProvided_returnsUserDetails() throws JSONException {
+        // Arrange
+//        String createUserJson = "{\n" +
+//                "    \"firstName\":\"Sergey\",\n" +
+//                "    \"lastName\":\"Kargopolov\",\n" +
+//                "    \"email\":\"test3@test.com\",\n" +
+//                "    \"password\":\"12345678\",\n" +
+//                "    \"repeatPassword\":\"12345678\"\n" +
+//                "}";
+
+        JSONObject userDetailsRequestJson = new JSONObject();
+        userDetailsRequestJson.put("firstName", "Sergey");
+        userDetailsRequestJson.put("lastName", "Kargopolov");
+        userDetailsRequestJson.put("email", "test@test.com");
+        userDetailsRequestJson.put("password","12345678");
+        userDetailsRequestJson.put("repeatPassword", "12345678");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> request = new HttpEntity<>(userDetailsRequestJson.toString(), headers);
+
+        // Act
+        ResponseEntity<UserRest> createdUserDetailsEntity = testRestTemplate.postForEntity("/users",
+                request,
+                UserRest.class);
+        UserRest createdUserDetails = createdUserDetailsEntity.getBody();
+
+        // Assert
+        Assertions.assertEquals(HttpStatus.OK, createdUserDetailsEntity.getStatusCode());
+        Assertions.assertEquals(userDetailsRequestJson.getString("firstName"),
+                createdUserDetails.getFirstName(),
+                "Returned user's first name seems to be incorrect");
+        Assertions.assertEquals(userDetailsRequestJson.getString("lastName"),
+                createdUserDetails.getLastName(),
+                "Returned user's last name seems to be incorrect");
+        Assertions.assertEquals(userDetailsRequestJson.getString("email"),
+                createdUserDetails.getEmail(),
+                "Returned user's email seems to be incorrect");
+        Assertions.assertFalse(createdUserDetails.getUserId().trim().isEmpty(),
+                "User id should not be empty");
+
     }
 }
